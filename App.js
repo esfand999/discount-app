9b148437e00ae2d6d09d340106f9e0d8ecf1e37b
Esfandyar <esfand999@gmail.com>
2021-05-22 20:30:15 +0500

    calculator function

--- App.js
+++ App.js
@@ -1,47 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput  } from 'react-native';
 
 
-export default function App() {
+function App() {
   const [originalPrice, setOriginalPrice] = useState('')
   const [discount, setDiscount] = useState('')
-  const [saved, setSaved] = useState('0.00')
-  const [finalPrice, setFinalPrice] = useState('0.00')
+  // const [saved, setSaved] = useState('0.00')
+  // const [finalPrice, setFinalPrice] = useState('0.00')
 
-  const calculate = () => {
-    if(originalPrice != '' && discount != ''){
-      var savedAmount = (originalPrice*discount)/100;
-      var finalPrice = originalPrice - savedAmount;
-      setSaved(savedAmount.toFixed(2))
-      setFinalPrice(finalPrice.toFixed(2))
+  // const calculate = () => {
+  //   if(originalPrice != '' && discount != ''){
+  //     var saved = (originalPrice*discount)/100;
+  //     var finalPrice = originalPrice - saved;
+  //     setSaved(saved.toFixed(2))
+  //     setFinalPrice(finalPrice.toFixed(2))
+  //   }
+  // }
+
+  const priceLimits = (num) => {
+    if (num > 0 || num == "") {
+      setOriginalPrice(num);
     }
   }
-
   
-
+  const discountLimits = (num) => {
+    if (num >= 0 && num <= 100) {
+      setDiscount(num);
+    }
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}><h1>Discount App</h1></Text>
 
       <TextInput style={styles.textField} 
-        onChangeText={setOriginalPrice} 
+        onChangeText = {priceLimits}
         value={originalPrice} 
         placeholder="Original Price"
-        placeholderTextColor="grey"/> 
+        keyboardType="numeric"
+        placeholderTextColor="grey"/>
       
       <TextInput style={styles.textField} 
-        onChangeText={setDiscount} 
+        onChangeText={discountLimits} 
         value={discount} 
         placeholder="Discount Percentage"
+        keyboardType="numeric"
         placeholderTextColor="grey"/>
 
-      <Text>You Save: {saved}</Text>
-      <Text>Final Price: {finalPrice}</Text>
+      <Text style={styles.result}>You Save: {((discount / 100 * originalPrice)).toFixed(2)}</Text>
+      <Text style={styles.result}>Final Price: {(originalPrice - (discount / 100) * originalPrice).toFixed(2)}</Text>
 
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -49,7 +59,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#080808',
+    backgroundColor: '#252525',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -59,15 +69,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     textShadowColor: '#6441a4',
     textShadowOffset: { width: 3, height: 2 },
-    textShadowRadius: 3,
-    
+    textShadowRadius: 3,  
   },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    
-  },
+  // input: {
+  //   height: 40,
+  //   margin: 12,
+  //   borderWidth: 1,
+  // },
   textField: {
     height: 50,
     width: 250,
@@ -76,11 +84,16 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     fontSize: 15,
     borderRadius: 2,
-    borderWidth: 3,
     color: 'white',
-    marginBottom: 15
+    marginBottom: 15,
+    backgroundColor: '#111111'
   },
-  calculations: {
-
+  result: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold'
   }
 });
+
+export default App;
+
